8b280a8bc958af1a83a002a82ab683521628769c
eyal4845 <eyal4845@gmail.com>
2023-08-18 16:05:14 +0300

    COMMENT

--- Nigzarot.py
+++ Nigzarot.py
@@ -47,6 +47,24 @@ def adish_handeling(string_one,string_two,adish) -> str:
     if string_two == adish:
         return string_one
     
+def first_part_exp_d(dg_x,f_x) -> str:
+    if is_number(f_x) and float(f_x) <= 0:
+        raise ValueError("Math Error")
+    if dg_x == "0":
+        return "0"
+    lan_f_x = str(np.log(float(f_x))) if is_number(f_x) else 'ln(' + f_x + ')'
+    if dg_x == "1" or lan_f_x == "1":
+        return adish_handeling(dg_x,lan_f_x,"1")
+    return numbers_handeling(dg_x,lan_f_x,"*")
+
+def second_part_exp_d(g_x,f_x,df_x):
+    if f_x == "0":
+        raise ValueError("Math Error")
+    mone = adish_handeling(g_x,df_x,"1") if g_x == "1" or df_x == "1" else numbers_handeling(g_x,df_x,"*")
+    if f_x == "1":
+        return mone
+    return numbers_handeling(mone,f_x,"/")
+    
 def plus_or_minus_handeling(df_x,dg_x,sign) -> str:
     if df_x == "0" or dg_x == "0":
         return adish_handeling(df_x,dg_x,"0")
@@ -73,6 +91,24 @@ def division_handeling(f_x,g_x,df_x,dg_x):
     mone = multiply_handeling(f_x,g_x,df_x,dg_x,"-")
     mechane = numbers_handeling(g_x,"2","^")
     return numbers_handeling(mone,mechane,"/")
+
+def exponent_handeling(f_x,g_x,df_x,dg_x) -> str:
+    if f_x == "0" and g_x == "0":
+        raise ValueError("Math Error")
+    if f_x == "0":
+        return "0"
+    if g_x == "0":
+        return "1"
+    if f_x == "1":
+        return "1"
+    if g_x == "1":
+        return f_x
+    start = numbers_handeling(f_x,g_x,"^")
+    first_part_exp = first_part_exp_d(dg_x,f_x)
+    second_part_exp = second_part_exp_d(g_x,f_x,df_x)
+    end = numbers_handeling(first_part_exp,second_part_exp,"+")
+    return numbers_handeling(start,end,"*")
+    
 
 def Diff(f) -> str:
     if f == 'sin(x)':
@@ -122,5 +158,8 @@ def Diff(f) -> str:
                 return multiply_handeling(f_x,g_x,Diff(f_x),Diff(g_x),"+")
             if sign == "/":
                 return division_handeling(f_x,g_x,Diff(f_x),Diff(g_x))
+            if sign == "^":
+                return exponent_handeling(f_x,g_x,Diff(f_x),Diff(g_x))
+    raise ValueError("Invalid function")
         
-print(Diff("(x/cos(x))"))
+print(Diff("(x/x)"))
