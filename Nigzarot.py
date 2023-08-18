5f873bf76e8790c69f5c23ca311032389a1e9755
eyal4845 <eyal4845@gmail.com>
2023-08-18 14:34:38 +0300

    my first commit

---
+++ Nigzarot.py
@@ -0,0 +1,138 @@
+import numpy as np
+
+CREATION_SIGNS = {'+','-','*','/','^'}
+
+NUMBERS = {'0','1','2','3','4','5','6','7','8','9'}
+
+B = {'sin(x)','cos(x)','tg(x)','arcsin(x)','arcsin(x)','arctg(x)','exp(x)','ln(x)','x'}
+
+B_DIFF = {"cos(x)","-sin(x)","1 / (cos(x) ** 2)","1 / sqrt(1 - x ** 2)","1 / (1 + x ** 2)","exp(x)","1 / x","0","1"}
+
+def is_number(string) -> bool:
+    global NUMBERS
+    if string[0] == '-':
+        string = string[1:]
+    if string[0] == '0':
+        return True if len(string) == 1 else False
+    dot_appeared = False
+    for i in range(len(string)):
+        if string[i] == ".":
+            if dot_appeared:
+             return False
+            else:
+             dot_appeared = True
+        elif string[i] not in NUMBERS:
+            return False
+    return True
+
+def numbers_handeling(string_one,string_two,sign) -> str:
+    if not is_number(string_one) or not is_number(string_two):
+        return "(" + string_one + sign + string_two + ")"
+    if sign == "+":
+        return str(float(string_one)+float(string_two))
+    if sign == "-":
+        return str(float(string_one)-float(string_two))
+    if sign == "*":
+        return str(float(string_one)*float(string_two))
+    if sign == "/":
+        return str(float(string_one)/float(string_two))
+    if sign == "^":
+        return str(float(string_one)**float(string_two))
+
+def adish_handeling(string_one,string_two,adish) -> str:
+    if string_one == adish:
+        return adish if string_two == adish else string_two
+    if string_two == adish:
+        return string_one
+    
+def plus_or_minus_handeling(df_x,dg_x,sign) -> str:
+    if df_x == "0" or dg_x == "0":
+        return adish_handeling(df_x,dg_x,"0")
+    return numbers_handeling(df_x,dg_x,sign)
+
+def multiply_handeling(f_x,g_x,df_x,dg_x,sign):
+    if df_x == "0" or g_x == "0":
+        if f_x == "0" or dg_x == "0":
+            return "0";
+        if f_x == "1":
+            return dg_x
+        if dg_x == "1":
+            return f_x
+        return numbers_handeling(f_x,dg_x,"*")
+    if f_x == "0" or dg_x == "0":
+        if df_x == "1":
+            return g_x
+        if g_x == "1":
+            return df_x
+        return numbers_handeling(df_x,g_x,"*")
+    if df_x == "1":
+        if f_x == "1":
+            return numbers_handeling(g_x,dg_x,sign)
+        if dg_x == "1":
+            return numbers_handeling(g_x,f_x,sign)
+    if g_x == "1":
+        if f_x == "1":
+            return numbers_handeling(df_x,dg_x,sign)
+        if dg_x == "1":
+            return numbers_handeling(df_x,f_x,sign)
+    part_one = numbers_handeling(df_x,g_x,"*")
+    part_two = numbers_handeling(f_x,dg_x,"*")
+    return numbers_handeling(part_one,part_two,sign)
+
+def division_handeling(f_x,g_x,df_x,dg_x):
+    if g_x == "0":
+        raise ValueError("Math Error")
+    mone = multiply_handeling(f_x,g_x,df_x,dg_x,"-")
+    mechane = numbers_handeling(g_x,"2","^")
+    return numbers_handeling(mone,mechane,"/")
+
+def Diff(f) -> str:
+    if f == 'sin(x)':
+        return "cos(x)"
+    if f == 'cos(x)':
+        return "-sin(x)"
+    if f == 'tg(x)':
+        return "1 / (cos(x) ** 2)"
+    if f == 'arcsin(x)':
+        return "1 / sqrt(1 - x ** 2)"
+    if f == 'arccos(x)':
+        return "-1 / sqrt(1 - x ** 2)"
+    if f == 'arctg(x)':
+        return "1 / (1 + x ** 2)"
+    if f == 'exp(x)':
+        return "exp(x)"
+    if f == 'ln(x)':
+        return "1 / x"
+    if is_number(f):
+        return "0"
+    if f == "x":
+        return "1"
+    if f[0] == "x" and f[1] == "^" and is_number(f[2:]):
+        if int(f[2:]) == 1:
+            return "1"
+        if int(f[2:]) == 2:
+            return "2*x"
+        return str(f[2:]) + "*x^" + str(int(f[2:])-1)
+    if f[0] != '(' or f[-1] != ')':
+        raise ValueError("Invalid function")
+    string = f[1:-1]
+    if string[0] == '-':
+        return '(' + '-' + Diff(string[1:]) + ')'
+    brackets = 0
+    for i in range(len(string)):
+        if string[i] == '(':
+            brackets += 1
+        if string[i] == ')':
+            brackets -= 1
+        if brackets == 0 and (string[i] in CREATION_SIGNS):
+            sign = string[i]
+            f_x = string[:i]
+            g_x = string[i+1:]
+            if sign in {'+', '-'}:
+                return plus_or_minus_handeling(Diff(f_x),Diff(g_x),sign)
+            if sign == "*":
+                return multiply_handeling(f_x,g_x,Diff(f_x),Diff(g_x),"+")
+            if sign == "/":
+                return division_handeling(f_x,g_x,Diff(f_x),Diff(g_x))
+        
+print(Diff("(x^2/cos(x))"))
