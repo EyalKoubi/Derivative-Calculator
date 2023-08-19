22294f6db068a0f37bae40a76b139e4d9653d33a
eyal4845 <eyal4845@gmail.com>
2023-08-19 15:40:50 +0300

    WORKING PROTOTIPE

--- Nigzarot.py
+++ Nigzarot.py
@@ -6,15 +6,42 @@ NUMBERS = {'0','1','2','3','4','5','6','7','8','9'}
 
 B = {'sin(x)','cos(x)','tg(x)','arcsin(x)','arcsin(x)','arctg(x)','exp(x)','ln(x)','x'}
 
-B_DIFF = {"cos(x)","-sin(x)","1 / (cos(x) ** 2)","1 / sqrt(1 - x ** 2)","1 / (1 + x ** 2)","exp(x)","1 / x","0","1"}
+B_DIFF = {"cos(x)","-sin(x)","(1 / (cos(x) ** 2))","(1 / sqrt(1 - x ** 2))","(1 / (1 + x ** 2))","exp(x)","(1 / x)","0","1"}
 
 def x_exists_handler(func,result,x):
     return result if x else func
 
-def plus_or_minus_eval_handeling(f,g,sign,x):
+def eval_minus_handeling(f,x):
     if x:
-        return f+g if sign == "+" else f-g
-    return lambda x: f(x)+g(x) if sign == "-" else lambda x: f(x)-g(x)
+        return -f
+    return lambda x: -f(x)
+
+def eval_creation_signs_handeling(f,g,sign,x):
+    if x:
+        if sign == "+":
+            return f+g
+        if sign == "-":
+            return f-g
+        if sign == "*":
+            return f*g
+        if sign == "/":
+            if g == 0:
+                raise ValueError("Math Error")
+            return f/g
+        if sign == "^":
+            if f == 0 and g == 0:
+                raise ValueError("MathError")
+            return f**g
+    if sign == "+":
+        return lambda x: f(x)+g(x)
+    if sign == "-":
+        return lambda x: f(x)-g(x)
+    if sign == "*":
+        return lambda x: f(x)*g(x)
+    if sign == "/":
+        return lambda x: f(x)/g(x)
+    if sign == "^":
+        return lambda x: f(x)**g(x)
 
 def is_number(string) -> bool:
     global NUMBERS
@@ -124,27 +151,21 @@ def Diff(f) -> str:
     if f == 'cos(x)':
         return "-sin(x)"
     if f == 'tg(x)':
-        return "1 / (cos(x) ** 2)"
+        return "(1 / (cos(x) ** 2))"
     if f == 'arcsin(x)':
-        return "1 / sqrt(1 - x ** 2)"
+        return "(1 / sqrt(1 - x ** 2))"
     if f == 'arccos(x)':
-        return "-1 / sqrt(1 - x ** 2)"
+        return "(-1 / sqrt(1 - x ** 2))"
     if f == 'arctg(x)':
-        return "1 / (1 + x ** 2)"
+        return "(1 / (1 + x ** 2))"
     if f == 'exp(x)':
         return "exp(x)"
     if f == 'ln(x)':
-        return "1 / x"
+        return "(1 / x)"
     if is_number(f):
         return "0"
     if f == "x":
         return "1"
-    if f[0] == "x" and f[1] == "^" and is_number(f[2:]):
-        if int(f[2:]) == 1:
-            return "1"
-        if int(f[2:]) == 2:
-            return "2*x"
-        return str(f[2:]) + "*x^" + str(int(f[2:])-1)
     if f[0] != '(' or f[-1] != ')':
         raise ValueError("Invalid function")
     string = f[1:-1]
@@ -186,16 +207,30 @@ def Eval(f, x=False) -> float:
     if f == 'exp(x)':
         return x_exists_handler(np.exp,np.exp(x),x)
     if f == 'ln(x)':
-        return x_exists_handler(np.ln,np.ln(x),x)
+        return x_exists_handler(np.log,np.log(x),x)
+    if f == "-sin(x)":
+        return x_exists_handler(lambda x: -np.sin(x),-np.sin(x),x)
+    if f == "(1 / (cos(x) ** 2))":
+        return x_exists_handler(lambda x: 1 / (np.cos(x) ** 2),1 / (np.cos(x) ** 2),x)
+    if f == "(1 / sqrt(1 - x ** 2))":
+        return x_exists_handler(lambda x: 1 / np.sqrt(1 - x ** 2),1 / np.sqrt(1 - x ** 2),x)
+    if f == "(1 / (1 + x ** 2))":
+        return x_exists_handler(lambda x: 1 / (1 + x ** 2),1 / (1 + x ** 2),x)
+    if f == "(1 / x)":
+        return x_exists_handler(lambda x: 1 / x,1 / x,x)
     if is_number(f):
-        return float(f)
+        return x_exists_handler(lambda x: float(f),float(f),x)
     if f == "x":
         return x_exists_handler(lambda x: x,x,x)
-    if f[0] == "x" and f[1] == "^" and is_number(f[2:]):
-        return x_exists_handler(lambda x: x**float(f[2:]),x**float(f[2:]),x)
+    if f == "(2*x)":
+        return x_exists_handler(lambda x: 2*x,2*x,x)
     string = f[1:-1]
+    print("f: ",f)
     if string[0] == '-':
-        return -Eval(string[1:],x)
+        st = string[1:]
+        if st not in B.union(B_DIFF) and (st[0] != '(' or st[-1] != ')'):
+            st = '(' + st + ')'
+        return eval_minus_handeling(Eval(st,x),x)
     brackets = 0
     for i in range(len(string)):
         if string[i] == '(':
@@ -206,17 +241,9 @@ def Eval(f, x=False) -> float:
             sign = string[i]
             f_x = string[:i]
             g_x = string[i+1:]
-            if sign in {'+', '-'}:
-                return plus_or_minus_eval_handeling(Eval(f_x,x),Eval(g_x,x),sign,x)
-            if sign == "*":
-                return multiply_handeling(f_x,g_x,Diff(f_x),Diff(g_x),"+")
-            if sign == "/":
-                return division_handeling(f_x,g_x,Diff(f_x),Diff(g_x))
-            if sign == "^":
-                return exponent_handeling(f_x,g_x,Diff(f_x),Diff(g_x))
+            return eval_creation_signs_handeling(Eval(f_x,x),Eval(g_x,x),sign,x)
     raise ValueError("Invalid function")
     
-        
-print(Diff("(x/x)"))
+print(Diff("(2*x)"))
 
-print(Eval("(x+x)")(2))
+print(Eval(Diff("x"),0.5))
